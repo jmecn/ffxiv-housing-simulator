76bfa599e45666562c4819913d5a2bc45a7f2d80
unknown <115050813@qq.com>
2021-08-29 02:18:50 +0800

    [M]补充HousingItemcateory

--- saint-coinach/src/main/java/ffxiv/housim/saintcoinach/db/xiv/entity/housing/HousingItemCategory.java
+++ saint-coinach/src/main/java/ffxiv/housim/saintcoinach/db/xiv/entity/housing/HousingItemCategory.java
@@ -3,15 +3,17 @@ package ffxiv.housim.saintcoinach.db.xiv.entity.housing;
 public enum HousingItemCategory {
 
 
-    ROOF(1, "房顶"),
-    EXTERIOR_WALL(2, "外墙"),
-    WINDOW(3, "窗户"),
-    DOOR(4, "门"),
+    ROOF(1, "房顶"),// rof
+    EXTERIOR_WALL(2, "外墙"),//
+    WINDOW(3, "窗户"),// wid
+    DOOR(4, "门"),// dor
+
+    RF(5, "烟囱"),// rf bgcommon/hou/dyna/opt/rf/%04d/assert/opt_rf_m%04d.sgb 0~8
+    SG(6, "门牌"),// sg bgcommon/hou/dyna/opt/sg/%04d/assert/opt_sg_m%04d.sgb 0~9
+    WL(7, "遮篷"),// wl bgcommon/hou/dyna/opt/wl/%04d/assert/opt_wl_m%04d.sgb 0~6
 
     FENCE(8, "栅栏"),
 
-    // 烟囱
-    // 遮篷
     INTERIOR_WALL(9, "内墙"),
     FLOOR(10, "地板"),
     LIGHT(11, "屋顶照明"),
@@ -23,6 +25,8 @@ public enum HousingItemCategory {
     Rug(16, "地毯"),
     OUTDOOR_FURNISHING(17, "庭具"),
 
+    // 18 cho bgcommon/hou/outdoor/cho/%04d/asset/cho_f%d_m%04d.sgb
+    // 19 frm bgcommon/hou/outdoor/frm/%04d/asset/frm_f%d_m%04d.sgb
     ;
 
     int value;
